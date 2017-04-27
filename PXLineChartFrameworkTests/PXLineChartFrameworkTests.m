a809d8b49a70a253dad1bf332276428a81a6db5a
彭欣 <pds296@126.com>
2017-04-27 09:36:01 +0800

    build framework

---
+++ PXLineChartFrameworkTests/PXLineChartFrameworkTests.m
@@ -0,0 +1,39 @@
+//
+//  PXLineChartFrameworkTests.m
+//  PXLineChartFrameworkTests
+//
+//  Created by Xin Peng on 17/4/27.
+//  Copyright © 2017年 EB. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface PXLineChartFrameworkTests : XCTestCase
+
+@end
+
+@implementation PXLineChartFrameworkTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
